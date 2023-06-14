fa5d33b8929ebe805c54d1cad508a12dba272232
Meera javad <meerajavad@gmail.com>
2023-06-14 22:00:04 +0530

    new commit

---
+++ src/main/java/com/ust/user/OnlineShoppingApplication.java
@@ -0,0 +1,19 @@
+package com.ust.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShoppingApplication.class, args);
+	}
+
+}
+
+
+
+
+
+
